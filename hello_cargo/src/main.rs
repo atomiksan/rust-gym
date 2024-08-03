f9eb9d47dac36e49455421949ca92e4ecce16e8f
atomiksan <nsofbl@gmail.com>
2024-08-04 02:44:12 +0530

    data types and others

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
+    let age = 27;
+    let name = "Happy";
+
+    println!("Hello {}! you are {} years old", name, age);
+
+    if age >= 18 {
+        println!("You are an adult");
+    } else {
+        println!("You are not an adult");
+    }
+
+    //looping
+    for i in 1..5 {
+        println!("Iteration Number: {}", i);
+    }
+
+    //Array
+    let arr: [i32; 4] = [1, 2, 3, 4];
+    for i in 0..4 {
+        println!("Array Value: {}", arr[i]);
+    }
+
+    //tuples
+    let tup = (500, "hi sena", true);
+    println!("{}", tup.0);
+    println!("{}", tup.1);
+    println!("{}", tup.2);
 }
