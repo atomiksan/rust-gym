82e6791c68bc0e55d386137316444e8261f8d7e9
atomiksan <nsofbl@gmail.com>
2024-08-18 23:44:11 +0530

    intro to functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     println!("hello world!");
-    another_function();
+    another_function(5, 'h');
 }
-fn another_function() {
+fn another_function(x: i32, y: char) {
     println!("another function!");
+    println!(
+        "the value of x & tag passed from main function is {}{}",
+        x, y
+    );
 }
