97b57aba5baa6277e8b9113a038245467b992aeb
atomiksan <nsofbl@gmail.com>
2024-11-07 16:08:41 +0530

    functions in rust

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("hello world!");
     another_function(5, 'h');
+    print_details("Mario likes chocolates");
+    println!("{}", gcd(20, 5));
 }
 fn another_function(x: i32, y: char) {
     println!("another function!");
@@ -8,4 +10,16 @@ fn another_function(x: i32, y: char) {
         "the value of x & tag passed from main function is {}{}",
         x, y
     );
+}
+fn print_details(s: &str) {
+    println!("{}", s);
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    a
 }
