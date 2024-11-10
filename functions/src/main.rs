a0d9fcf36faf670a202caa4679055e2d0914e1d7
atomiksan <nsofbl@gmail.com>
2024-11-10 16:09:03 +0530

    functions in rust

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
     another_function(5, 'h');
     print_details("Mario likes chocolates");
     println!("{}", gcd(20, 5));
+    println!("{}", multiple_return_values(true));
+    println!("{}", multiple_return_values(false));
+    let s = "Hello".to_string();
+    println!("{}", concat_string(&s));
 }
 fn another_function(x: i32, y: char) {
     println!("another function!");
@@ -22,4 +26,17 @@ fn gcd(mut a: u64, mut b: u64) -> u64 {
         b = remainder;
     }
     a
+}
+
+fn multiple_return_values(flag: bool) -> bool {
+    if flag == true {
+        true
+    } else {
+        false
+    }
+}
+
+fn concat_string(s: &str) -> String {
+    let s2 = " World".to_string();
+    s.to_owned() + &s2
 }
